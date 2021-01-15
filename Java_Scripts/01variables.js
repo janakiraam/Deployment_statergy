6502362dd83e0300bcee88df3023a3d5f08bfd72
janakiraam <jnk266raam@gmail.com>
2021-01-15 23:19:40 +0530

    Add files via upload

---
+++ Java_Scripts/01variables.js
@@ -0,0 +1,26 @@
+var fullName = "Janaki Raam";
+
+// how to declare a variable
+
+// fullName
+// full-Name
+// full_Name
+// FullName
+
+var courseName = "Recat JS Bootcamp";
+var isLoggedIN = true;
+
+// ture and false is boolean variable and different from True and False
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+
+console.log(paymentMode);
+
+paymentMode = "Credit card";
+
+// var if=23; ==> can't use as if is a reserved keyword
