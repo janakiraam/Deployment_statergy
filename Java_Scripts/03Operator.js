b2d092f5fd6df135469ef09421797ce4e55daf54
janakiraam <jnk266raam@gmail.com>
2021-01-17 23:53:51 +0530

    Add files via upload

---
+++ Java_Scripts/03Operator.js
@@ -0,0 +1,32 @@
+var num1 = 7;
+var num2 =5;
+
+console.log(num1+num2);
+
+var answer = num1>num2;
+console.log(answer);
+
+// D = (L - S)/L *100
+
+var listingPrice = 799;
+var sellingPrice = 199;
+
+var percentage = ((listingPrice - sellingPrice)/ listingPrice)*100;
+console.log(percentage);
+
+displayPercentage = Math.round(percentage);  // Math is inbulid function
+console.log(displayPercentage)
+
+// salary calculation
+
+var oldSalary = 10000
+var newSalary = 12000
+var percentage = ((newSalary-oldSalary)/ oldSalary)*100;
+console.log(percentage)
+
+var result = newSalary > oldSalary
+console.log(result)
+
+// using typeof will give you result data type
+
+console.log (typeof result)
